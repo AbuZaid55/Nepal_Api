dbc87a23e98e3afb3a8b0d5671e9952987d8dcd9
Abu Zaid <zaid70979@gmail.com>
2023-09-20 18:54:31 +0530

    fix js code

---
+++ script.js
@@ -0,0 +1,64 @@
+const container= document.querySelector('.container')
+const request = new XMLHttpRequest()
+request.open('get','https://restcountries.com/v2/name/nepal')
+request.send()
+request.addEventListener('load',function(){
+    const [data] = JSON.parse(this.responseText);
+    const htmlData = `<div class="card">
+    <article class="card">
+        <div class="card-body">
+            <img src="${data.flag}" alt="" class="card-body-img">
+            <h1 class="card-body-title">
+                ${data.name}<span></span>
+            </h1>
+            <p class="card-body-text">capital:${data.capital}</p>
+        </div>
+        <div class="card-footer">
+            <div class="card-footer-social">
+                <h3>${data.nativeName}</h3>
+                <p>Native Language</p>
+            </div>
+            <div class="card-footer-social">
+                <h3>${data.population}</h3>
+                <p>Population</p>
+            </div>
+            <div class="card-footer-social">
+                <h3>${data.demonym}</h3>
+                <p>Demonym</p>
+            </div>
+        </div>
+    </article>
+</div>`
+container.insertAdjacentHTML("afterbegin",htmlData)
+})
+
+
+
+
+
+
+
+
+
+
+
+//
+// const container= document.querySelector('.container')
+// const request = new XMLHttpRequest()
+// request.open('get','https://restcountries.com/v2/name/nepal')
+// request.send()
+// request.addEventListener('load',function(){
+//     const [data] = JSON.parse(this.responseText);
+//     const htmlData = `<div class="card">
+//     <article class="card">
+//         <div class="card-body">
+//             <img src="${data.flag}" alt="" class="card-body-img">
+//             <h1 class="card-body-title">
+//                 ${data.name}<span></span>
+//             </h1>
+//             <p class="card-body-text">capital:${data.capital}</p>
+//         </div>
+//     </article>
+// </div>`
+// container.insertAdjacentHTML("afterbegin",htmlData)
+// })
